00e08440094e10f2dabb41b39803950d498249e2
dylanjhaw <dhawryluk2020@fau.edu>
2024-01-26 14:56:55 -0500

    Removed commented code

--- timetabled/src/components/Event.jsx
+++ timetabled/src/components/Event.jsx
@@ -1,27 +1,4 @@
-// import React from "react";
 
-// import riverweb from '../assets/riverweb.jpeg';
-
-
-// const Event = (props) => {
-
-//     return (
-//             <td className={'Event ' + props.color}>
-//                 <img src={props.photo} alt="Event" style={{ width: '200px', height: '150px' }}/>
-//                 <div className="event-content">
-//                     <h2>Title</h2>
-//                     <p>Description</p>
-//                 </div>
-//                 <h5>{props.event}</h5>
-//                 <h6>{props.location}</h6>
-//             </td>
-//         )
-  
-// }
-
-
-
-// export default Event;
 
 import React from "react";
 
